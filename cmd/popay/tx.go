141b9a699812d2c7bf8ba62f6ed6645afbb7f8eb
jhamPac <cloud@kevia.me>
2020-05-18 08:30:21 -0700

    added tx command arguments

---
+++ cmd/popay/tx.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jhampac/pueblo/database"
+	"github.com/spf13/cobra"
+)
+
+const flagFrom = "from"
+const flagTo = "to"
+const flagValue = "value"
+const flagData = "data"
+
+func txCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tx",
+		Short: "Interact with transactions (add ...)",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return incorrectUsageErr()
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+
+		},
+	}
+
+	cmd.AddCommand(txsAddCmd())
+
+	return cmd
+}
+
+func txsAddCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "add",
+		Short: "Adds new TX to database",
+		Run: func(cmd *cobra.Command, args []string) {
+			from, _ := cmd.Flags().GetString(flagFrom)
+			to, _ := cmd.Flags().GetString(flagTo)
+			value, _ := cmd.Flags.GetUint(flagValue)
+
+			fromAcc := database.NewAccount(from)
+			toAcc := database.NewAccount(to)
+
+			tx := database.NewTx(fromAcc, toAcc, value, "")
+
+			state, err := database.NewStateFromDisk()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			defer state.Close()
+
+			if err := state.Add(tx); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			if err := state.Persist(); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			fmt.Println("TX sucessfully added to the ledger!")
+		},
+	}
+
+	cmd.Flags().String(flagFrom, "", "From what account to send tokens")
+	cmd.MarkFlagRequired(flagFrom)
+
+	cmd.Flags.String(flagTo, "", "To what account to send tokens")
+	cmd.MarkFlagRequired(flagTo)
+
+	cmd.Flags().Uint(flagValue, 0, "How many tokens to send")
+	cmd.MarkFlagRequired(flagValue)
+
+	return cmd
+}
