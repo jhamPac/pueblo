235d3f8824847b4d7ab9c3ea3c0e66761bc1ea53
jhamPac <cloud@kevia.me>
2020-05-17 17:04:28 -0700

    created a cli folder

---
+++ cmd/popay/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var cmd = &cobra.Command{
+		Use:   "pueblo",
+		Short: "Random name for a blockchain",
+		Run:   func(cmd *cobra.Command, args []string) {},
+	}
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jhampac/pueblo
+
+go 1.14
