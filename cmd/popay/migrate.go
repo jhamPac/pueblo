1bdf15c7c834cfc2ae7326df9444907ad268cc4a
jhamPac <cloud@kevia.me>
2020-05-29 18:44:40 -0700

    saving before trying out the migrate command

--- cmd/popay/migrate.go
+++ cmd/popay/migrate.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/jhampac/pueblo/database"
+	"github.com/spf13/cobra"
 )
 
 var migrate = &cobra.Command{
-	Use: "migrate",
+	Use:   "migrate",
 	Short: "migrate tx to block database",
 	Run: func(cmd *cobra.Command, args []string) {
 		state, err := database.NewStateFromDisk()
@@ -14,5 +18,39 @@ var migrate = &cobra.Command{
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-	}
+		state.Close()
+
+		block0 := database.NewBlock(
+			database.Hash{},
+			uint64(time.Now().Unix()),
+			[]database.Tx{
+				database.NewTx("redcloud", "redcloud", 3, ""),
+				database.NewTx("redcloud", "redcloud", 700, "reward"),
+			},
+		)
+
+		state.AddBlock(block0)
+		block0hash, _ := state.Persist()
+
+		block1 := database.NewBlock(
+			block0hash,
+			uint64(time.Now().Unix()),
+			[]database.Tx{
+				database.NewTx("redcloud", "sittingbull", 2000, ""),
+				database.NewTx("redcloud", "redcloud", 100, "reward"),
+				database.NewTx("sittingbull", "redcloud", 10, ""),
+				database.NewTx("sittingbull", "woundedknee", 1000, ""),
+				database.NewTx("sittingbull", "redcloud", 50, ""),
+				database.NewTx("redcloud", "redcloud", 700, "reward"),
+			},
+		)
+
+		state.AddBlock(block1)
+
+		_, err = state.Persist()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	},
 }
