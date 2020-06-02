e0ca284ad64daabf132adae2fe8796a5f28f08b7
jhamPac <cloud@kevia.me>
2020-06-01 18:41:08 -0700

    save point; need to update some functions in state.go

--- cmd/popay/balances.go
+++ cmd/popay/balances.go
@@ -23,22 +23,29 @@ func balancesCmd() *cobra.Command {
 	return cmd
 }
 
-var balancesListCmd = &cobra.Command{
-	Use:   "list",
-	Short: "lists all balances",
-	Run: func(cmd *cobra.Command, args []string) {
-		state, err := database.NewStateFromDisk()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		defer state.Close()
+func balancesListCmd() *cobra.Command {
+	var blCmd = *cobra.Command{
+		Use:   "list",
+		Short: "lists all balances",
+		Run: func(cmd *cobra.Command, args []string) {
+			dataDir, _ := cmd.Flags()GetString(flagDataDir)
+			state, err := database.NewStateFromDisk(dataDir)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			defer state.Close()
 
-		fmt.Printf("Accounts balances at %x:\n", state.LatestBlockHash())
-		fmt.Println("------------------------")
+			fmt.Printf("Accounts balances at %x:\n", state.LatestBlockHash())
+			fmt.Println("------------------------")
 
-		for account, balance := range state.Balances {
-			fmt.Println(fmt.Sprintf("%s: %d", account, balance))
-		}
-	},
+			for account, balance := range state.Balances {
+				fmt.Println(fmt.Sprintf("%s: %d", account, balance))
+			}
+		},
+	}
+
+	addDefaultRequiredFlags(blCmd)
+
+	return blCmd
 }
