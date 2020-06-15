2d520296f1924c54cf2e49befd5252bc12403b44
jhamPac <cloud@kevia.me>
2020-06-15 16:38:07 -0700

    dynamically setting path to db

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"log"
+	"path/filepath"
+	"runtime"
 
 	"github.com/jhampac/pueblo/node"
 )
 
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basePath   = filepath.Dir(b)
+)
+
 func main() {
-	if err := node.Run("pwd"); err != nil {
+	if err := node.Run(basePath + "/database/.db"); err != nil {
 		log.Fatalf("An error as occured: %v", err)
 	}
+
 }
