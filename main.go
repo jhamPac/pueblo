4b54411a8992b9d9e7e477c8d444c3e231b95b99
jhamPac <cloud@kevia.me>
2020-06-15 19:49:03 -0700

    config files for GCP is giving me a headache

--- main.go
+++ main.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"path/filepath"
-	"runtime"
-
-	"github.com/jhampac/pueblo/node"
+	"net/http"
+	"os"
 )
 
-var (
-	_, b, _, _ = runtime.Caller(0)
-	basePath   = filepath.Dir(b)
-)
+// import (
+// 	"log"
+// 	"path/filepath"
+// 	"runtime"
+
+// 	"github.com/jhampac/pueblo/node"
+// )
+
+// var (
+// 	_, b, _, _ = runtime.Caller(0)
+// 	basePath   = filepath.Dir(b)
+// )
+
+// func main() {
+// 	if err := node.Run(basePath + "/database/.db"); err != nil {
+// 		log.Fatalf("An error as occured: %v", err)
+// 	}
+
+// }
+
+var secret string
 
 func main() {
-	if err := node.Run(basePath + "/database/.db"); err != nil {
-		log.Fatalf("An error as occured: %v", err)
+	secret = os.Getenv("SECRET")
+	http.HandleFunc("/", indexHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9000"
+		log.Printf("Defaulting to port %s", port)
 	}
 
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "The secret is %v", secret)
 }
