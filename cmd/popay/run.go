98f77a3f29d30b7cef13d81c82c3b1131cb656f7
jhamPac <cloud@kevia.me>
2020-06-23 18:57:09 -0700

    added dataDir retriever to all cmd files

--- cmd/popay/run.go
+++ cmd/popay/run.go
@@ -13,7 +13,6 @@ func runCmd() *cobra.Command {
 		Use:   "run",
 		Short: "Launches the Popay node and its HTTP API.",
 		Run: func(cmd *cobra.Command, args []string) {
-			dataDir, _ := cmd.Flags().GetString(flagDataDir)
 			port, _ := cmd.Flags().GetUint64(flagPort)
 
 			fmt.Println("Launching Popay node and its HTTP API...")
@@ -25,7 +24,7 @@ func runCmd() *cobra.Command {
 				true,
 			)
 
-			n := node.New(dataDir, port, bootstrap)
+			n := node.New(getDataDirFromCmd(cmd), port, bootstrap)
 
 			err := n.Run()
 			if err != nil {
