3367b0e54f90fd995adaa5e0ed48c61659a9a692
jhamPac <cloud@kevia.me>
2020-06-23 19:04:10 -0700

    created syncing algo; read only for now

---
+++ database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+)
+
+// GetBlocksAfter syncs with all blocks after a specified Hash
+func GetBlocksAfter(blockHash Hash, dataDir string) ([]Block, error) {
+	f, err := os.OpenFile(getBlocksDbFilePath(dataDir), os.O_RDONLY, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	blocks := make([]Block, 0)
+	shouldStartCollecting := false
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		var blockFS BlockFS
+		err = json.Unmarshal(scanner.Bytes(), &blockFS)
+		if err != nil {
+			return nil, err
+		}
+
+		if shouldStartCollecting {
+			blocks = append(blocks, blockFS.Value)
+			continue
+		}
+
+		if blockHash == blockFS.Key {
+			shouldStartCollecting = true
+		}
+	}
+	return blocks, nil
+}
